d58a5c8ff449de28bbe4b681a2ceffbac436553a
Imen Chabchoub <ichabchoub85@gmail.com>
2025-11-19 16:38:52 +0100

    swagger doc

--- src/main/java/com/example/suivi_livraison/controller/mobile/PreuveLivraisonController.java
+++ src/main/java/com/example/suivi_livraison/controller/mobile/PreuveLivraisonController.java
@@ -3,6 +3,7 @@ package com.example.suivi_livraison.controller.mobile;
 
 import com.example.suivi_livraison.Services.PreuveLivraisonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,10 +20,22 @@ public class PreuveLivraisonController {
     @Autowired
     private PreuveLivraisonService preuveLivraisonService;
 
-    @PostMapping
+    /*@PostMapping
     public ResponseEntity<?> uploadPreuve(
         @RequestParam Long idLivraison,
         @RequestParam("file") MultipartFile file // pour une image ou doc
+    ) {
+        boolean success = preuveLivraisonService.savePreuve(idLivraison, file);
+        if (success) {
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.badRequest().body("Echec upload");
+        }
+    }*/
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<?> uploadPreuve(
+        @RequestParam Long idLivraison,
+        @RequestParam("file") MultipartFile file
     ) {
         boolean success = preuveLivraisonService.savePreuve(idLivraison, file);
         if (success) {
